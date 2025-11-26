2569e78e38ab384f6ed979cd7883d02e2d7da5e8
Muhamed Cicak <silidrone@gmail.com>
2025-11-26 18:34:13 +0000

    added up and down for prod

--- down.sh
+++ down.sh
@@ -1,1 +1,7 @@
-docker compose -p siliweb down
+#!/bin/sh
+
+if [ "$1" = "--prod" ]; then
+  docker compose -f docker-compose.prod.yml -p siliweb down --remove-orphans
+else
+  docker compose -p siliweb down
+fi
--- up.sh
+++ up.sh
@@ -1,5 +1,13 @@
+#!/bin/sh
+
 export UID=$(id -u)
 export GID=$(id -g)
-
 cp .env backend/.env
-docker compose -p siliweb up --build
+
+if [ "$1" = "--prod" ]; then
+  docker network create siliweb 2>/dev/null
+  docker network create traefik 2>/dev/null
+  docker compose -f docker-compose.prod.yml -p siliweb up --build -d
+else
+  docker compose -p siliweb up --build
+fi
