19baad2ac49a8ad128ab3395f8ba11cfbb59b16f
Muhamed Cicak <silidrone@gmail.com>
2025-11-26 18:42:53 +0000

    remove run in bg mode for prod

--- up.sh
+++ up.sh
@@ -7,7 +7,7 @@ cp .env backend/.env
 if [ "$1" = "--prod" ]; then
   docker network create siliweb 2>/dev/null
   docker network create traefik 2>/dev/null
-  docker compose -f docker-compose.prod.yml -p siliweb up --build -d
+  docker compose -f docker-compose.prod.yml -p siliweb up --build
 else
   docker compose -p siliweb up --build
 fi
